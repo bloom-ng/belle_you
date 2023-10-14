34b2e02581995006b3ad93e12233fb84ed5cc79a
David Aremu <segun8427@gmail.com>
2023-10-14 16:00:17 +0100

    deleted old migrations file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,6 @@ return new class extends Migration {
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('remember_token', 100)->nullable();
-            $table->text('two_factor_secret')->nullable();
-            $table->text('two_factor_recovery_codes')->nullable();
-            $table->timestamp('two_factor_confirmed_at')->nullable();
             $table->text('profile_photo_path')->nullable();
             $table->boolean('is_distributor')->default(false);
 
